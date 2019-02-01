46ffd9139c414f42524557ca92a277b2539a31b5
Brady <brady.y.liu@gmail.com>
2019-01-31 18:17:30 -0800

    Worked on Psuedo refacto

--- src/blinkyDancer.js
+++ src/blinkyDancer.js
@@ -11,7 +11,7 @@ makeBlinkyDancer.prototype.step = function() {
   // toggle() is a jQuery method to show/hide the <span> tag.
   // See http://api.jquery.com/category/effects/ for this and
   // other effects you can use on a jQuery-wrapped html tag.
-  console.log(this.$node);
+  // console.log(this.$node);
   this.$node.toggle();
 };
 var oldStep = makeBlinkyDancer.prototype.step;
