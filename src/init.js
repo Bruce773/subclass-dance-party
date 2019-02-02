66734744978432782132b16132128fe5a1d840ef
Bruce Johnson <bruce@yogainternational.com>
2019-02-02 15:47:02 -0500

    updates

--- src/init.js
+++ src/init.js
@@ -1,13 +1,9 @@
-$(document).ready(function () {
+$(document).ready(function() {
   window.dancers = [];
   window.schoolOfFish = [];
   window.sharks = [];
 
-<<<<<<< HEAD
-  $(".addDancerButton").on("click", function(event) {
-=======
-  $('.addDancerButton').on('click', function (event) {
->>>>>>> subclass/master
+  $('.addDancerButton').on('click', function(event) {
     /* This function sets up the click handlers for the create-dancer
      * buttons on dancefloor.html. You should only need to make one small change to it.
      * As long as the "data-dancer-maker-function-name" attribute of a
@@ -21,7 +17,7 @@ $(document).ready(function () {
      * A new object of the given type will be created and added
      * to the stage.
      */
-    var dancerMakerFunctionName = $(this).data("dancer-maker-function-name");
+    var dancerMakerFunctionName = $(this).data('dancer-maker-function-name');
 
     // get the maker function for the kind of dancer we're supposed to make
     var dancerMakerFunction = window[dancerMakerFunctionName];
@@ -29,77 +25,67 @@ $(document).ready(function () {
     // make a dancer with a random position
 
     var dancer = new dancerMakerFunction(
-      $("body").height() * Math.random(),
-      $("body").width() * Math.random(),
+      $('body').height() * Math.random(),
+      $('body').width() * Math.random(),
       Math.random() * 1000
     );
-    $("body").append(dancer.$node);
+    $('body').append(dancer.$node);
   });
 
-<<<<<<< HEAD
-  $(".addFishButton").on("click", function(event) {
-    var makeFishName = $(this).data("create-fish");
-=======
-  $('.addFishButton').on('click', function (event) {
+  $('.addFishButton').on('click', function(event) {
     var makeFishName = $(this).data('create-fish');
->>>>>>> subclass/master
     var makeFishFunc = window[makeFishName];
     var fish = new makeFishFunc(
-      $("body").height() * Math.random(),
-      $("body").width() * Math.random()
+      $('body').height() * Math.random(),
+      $('body').width() * Math.random()
     );
-<<<<<<< HEAD
-    $("body").append(fish.$node);
-  });
-  $(".addSharkButton").on("click", function(event) {
-    var makeSharkName = $(this).data("create-shark");
-=======
     window.schoolOfFish.push(fish);
     $('body').append(fish.$node);
   });
-  $('.addSharkButton').on('click', function (event) {
+  $('.addSharkButton').on('click', function(event) {
     var makeSharkName = $(this).data('create-shark');
->>>>>>> subclass/master
     var makeSharkFunc = window[makeSharkName];
     var shark = new makeSharkFunc(
-      $("body").height() * Math.random(),
-      $("body").width() * Math.random()
+      $('body').height() * Math.random(),
+      $('body').width() * Math.random()
     );
-<<<<<<< HEAD
-    $("body").append(shark.$node);
-=======
     window.sharks.push(shark);
     $('body').append(shark.$node);
->>>>>>> subclass/master
   });
 
-  $('.lineUp').on('click', function (event) {
+  $('.lineUp').on('click', function(event) {
     //create a counter var
     let distanceBetweenEachCounter = 0;
     // iterate over schoolOfFish and sharks
     // console.log(window.schoolOfFish);
-    window.schoolOfFish.forEach(function (item) {
+    window.schoolOfFish.forEach(function(item) {
       //set item.$node.setPosition(distanceBetweenEachCounter + 5 , window.innerHeight/2)
-      item.setPosition( window.innerHeight / 10, distanceBetweenEachCounter + 10);
+      item.setPosition(
+        window.innerHeight / 10,
+        distanceBetweenEachCounter + 10
+      );
       distanceBetweenEachCounter += 20;
     });
-    window.sharks.forEach(function (item) {
+    window.sharks.forEach(function(item) {
       //set item.$node.setPosition(distanceBetweenEachCounter + 5 , window.innerHeight/2)
-      item.setPosition( window.innerHeight / 10, distanceBetweenEachCounter + 10);
+      item.setPosition(
+        window.innerHeight / 10,
+        distanceBetweenEachCounter + 10
+      );
       distanceBetweenEachCounter += 20;
     });
-  })
+  });
   $('.faceOff').on('click', function(event) {
     let faceOffCount = 0;
-    window.schoolOfFish.forEach(function (item) {
+    window.schoolOfFish.forEach(function(item) {
       //set item.$node.setPosition(distanceBetweenEachCounter + 5 , window.innerHeight/2)
       item.setPosition(faceOffCount + 100, window.innerHeight / 10);
       faceOffCount += 20;
     });
-    window.sharks.forEach(function (item) {
+    window.sharks.forEach(function(item) {
       //set item.$node.setPosition(distanceBetweenEachCounter + 5 , window.innerHeight/2)
       item.setPosition(faceOffCount + 10, window.innerHeight * 1.8);
       faceOffCount += 20;
     });
-  })
+  });
 });
